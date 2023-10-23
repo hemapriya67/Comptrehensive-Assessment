c84b167fa558479d6762abfd2b7ad12a8f5bfb14
hemapriya67 <148714459+hemapriya67@users.noreply.github.com>
2023-10-23 14:22:15 +0000

    import

--- src/main/com/test/MMT/ChromeDriver.java
+++ src/main/com/test/MMT/ChromeDriver.java
@@ -1,6 +1,16 @@
 package main.com.test.MMT;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class ChromeDriver{
     // Calling Chrome and opening make my trip
--- src/main/com/test/MMT/UtilityMethods.java
+++ src/main/com/test/MMT/UtilityMethods.java
@@ -1,6 +1,15 @@
 package main.com.test.MMT;
 
 import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UtilityMethods{
      WebDriverWait webDriverWait1;
